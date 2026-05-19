45ecee258b27520f39cf117ae5a8d14ab0aa4776
maland22 <maland22@htl-kaindorf.at>
2026-05-19 13:45:17 +0200

    inital Commit

---
+++ src/main/java/raingroup/climaterainproject/ClimateRainProjectApplication.java
@@ -0,0 +1,13 @@
+package raingroup.climaterainproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateRainProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimateRainProjectApplication.class, args);
+    }
+
+}
