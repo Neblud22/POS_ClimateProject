f3411e48d42d4707d5179eac5087fd26422f914d
neblud22 <neblud22@htl-kaindorf.at>
2026-05-26 13:16:52 +0200

    FileWriter added

---
+++ src/main/java/raingroup/climaterainproject/IO/FileWriter.java
@@ -0,0 +1,30 @@
+package raingroup.climaterainproject.IO;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+public class FileWriter {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public String save(Parameter parameter, String jsonResponse) throws Exception {
+
+        // folder
+        File outputDir = new File("output");
+        if (!outputDir.exists()) {
+            outputDir.mkdirs();
+        }
+
+        // objectmapper
+        Object json = objectMapper.readValue(jsonResponse, Object.class);
+        String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+
+        // write
+        File file = new File(parameter.toFileName());
+        java.io.FileWriter writer = new java.io.FileWriter(file);
+        writer.write(prettyJson);
+        writer.close();
+
+        return "Saved file: " + file.getAbsolutePath();
+    }
+}
