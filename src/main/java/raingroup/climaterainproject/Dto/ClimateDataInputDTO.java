ff9dafd4b3fac046dcf67ef6851f1a2054e3a3e7
neblud22 <neblud22@htl-kaindorf.at>
2026-06-07 22:11:09 +0200

    inputdto commit

---
+++ src/main/java/raingroup/climaterainproject/Dto/ClimateDataInputDTO.java
@@ -0,0 +1,19 @@
+package raingroup.climaterainproject.Dto;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class ClimateDataInputDTO {
+    private String startdate;
+    private String enddate;
+    private double longitude;
+    private double latitude;
+
+    // { "PRECTOTCORR": { "20250101": 0.0}, "RH2M": {}, "T2M": {} }
+    private Map<String, Map<String, Double>> measurements;
+
+    // { "PRECTOTCORR": { "units": "mm/day", "longname": "Precipitation Corrected" }, ... }
+    private Map<String, Map<String, String>> measurementInfo;
+}
