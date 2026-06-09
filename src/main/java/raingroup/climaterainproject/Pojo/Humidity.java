524a3dba66e67a0d8766663711c8b624240c91ee
neblud22 <neblud22@htl-kaindorf.at>
2026-06-09 14:43:35 +0200

    humidity entity created

---
+++ src/main/java/raingroup/climaterainproject/Pojo/Humidity.java
@@ -0,0 +1,33 @@
+package raingroup.climaterainproject.Pojo;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Humiditiy")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Humidity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "startdate", nullable = false)
+    private String startdate;
+
+    @Column(name = "enddate", nullable = false)
+    private String enddate;
+
+    @Column(name = "longitude", nullable = false)
+    private double longitude;
+
+    @Column(name = "latitude", nullable = false)
+    private double latitude;
+
+    @Column(name = "measurement", nullable = false)
+    private Double measurements;
+}
