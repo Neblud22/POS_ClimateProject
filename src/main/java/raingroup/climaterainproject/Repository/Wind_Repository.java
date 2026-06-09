30f4e6a2d0e22e3ae0c9f96bfb638ae154038718
maland22 <maland22@htl-kaindorf.at>
2026-06-09 14:48:33 +0200

    Commit Wind  Repository

---
+++ src/main/java/raingroup/climaterainproject/Repository/Wind_Repository.java
@@ -0,0 +1,7 @@
+package raingroup.climaterainproject.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import raingroup.climaterainproject.Pojo.Wind;
+
+public interface Wind_Repository extends JpaRepository <Wind, Long> {
+}
