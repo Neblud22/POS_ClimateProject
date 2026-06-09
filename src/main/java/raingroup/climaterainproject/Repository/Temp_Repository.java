fa9cb58e9476f584cd1e4bf6dde91576763b79d4
neblud22 <neblud22@htl-kaindorf.at>
2026-06-09 19:01:45 +0200

    repository for temp added

--- src/main/java/raingroup/climaterainproject/Repository/Temp_Repository.java
+++ src/main/java/raingroup/climaterainproject/Repository/Temp_Repository.java
@@ -1,7 +1,11 @@
 package raingroup.climaterainproject.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import raingroup.climaterainproject.Pojo.Temperature;
+import java.util.List;
 
+@Repository
 public interface Temp_Repository extends JpaRepository<Temperature, Long> {
+    List<Temperature> findByDate(String date);
 }
