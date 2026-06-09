dc6dec20369ba9f710cd01b88cda9ec2f0776847
neblud22 <neblud22@htl-kaindorf.at>
2026-06-09 14:45:02 +0200

    wind entity created

---
+++ src/main/java/raingroup/climaterainproject/Pojo/Wind.java
@@ -0,0 +1,32 @@
+package raingroup.climaterainproject.Pojo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Wind")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Wind {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "startdate", nullable = false)
+    private String startdate;
+
+    @Column(name = "enddate", nullable = false)
+    private String enddate;
+
+    @Column(name = "longitude", nullable = false)
+    private double longitude;
+
+    @Column(name = "latitude", nullable = false)
+    private double latitude;
+
+    @Column(name = "measurement", nullable = false)
+    private Double measurements;
+}
