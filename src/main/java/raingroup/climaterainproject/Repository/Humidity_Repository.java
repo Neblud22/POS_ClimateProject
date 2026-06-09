e1c664567eea3c4971b5d5c31563737cae9d192a
neblud22 <neblud22@htl-kaindorf.at>
2026-06-09 19:02:17 +0200

    repository for humidity added

--- src/main/java/raingroup/climaterainproject/Repository/Humidity_Repository.java
+++ src/main/java/raingroup/climaterainproject/Repository/Humidity_Repository.java
@@ -1,7 +1,13 @@
 package raingroup.climaterainproject.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import raingroup.climaterainproject.Pojo.Humidity;
 
+import java.util.List;
+
+@Repository
 public interface Humidity_Repository extends JpaRepository<Humidity,Long> {
+    List<Humidity> findByDate(String date);
+
 }
