d8f506861cb2bf9b9b7541c45f48dfeef8dbdd4c
neblud22 <neblud22@htl-kaindorf.at>
2026-06-09 19:01:31 +0200

    repository for wind added

--- src/main/java/raingroup/climaterainproject/Repository/Wind_Repository.java
+++ src/main/java/raingroup/climaterainproject/Repository/Wind_Repository.java
@@ -1,7 +1,12 @@
 package raingroup.climaterainproject.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import raingroup.climaterainproject.Pojo.Wind;
+import java.util.List;
 
+@Repository
 public interface Wind_Repository extends JpaRepository <Wind, Long> {
+    List<Wind> findByDate(String date);
+
 }
